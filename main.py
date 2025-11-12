6fdc80aa5fa7f26997c5556042cd2f4a37e6c15e
Alan Dunzz <dunzzlla@ualberta.ca>
2025-11-12 13:34:21 -0600

    Adding requirements file and updated code

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from role-of-lambda!")
+
+
+if __name__ == "__main__":
+    main()
--- test_ppo_parallel.py
+++ test_ppo_parallel.py
@@ -6,22 +6,11 @@ def test_ppo_lambda(env, gae_lambda, seed):
     terminal_size = shutil.get_terminal_size().columns
     print(fr"PPO env: {env} seed: {seed} lambda: {gae_lambda}".center(terminal_size,'-'))
     subprocess.run(
-        fr"python cleanrl/ppo.py --seed {seed} --gae_lambda {gae_lambda} --env-id {env} --total-timesteps 100000",
+        fr"python ppo.py --track --seed {seed} --gae_lambda {gae_lambda} --env-id {env} --total-timesteps 100000",
         shell=True,
         check=True
     )
 
-lambdas = [0.95, 0.9]  # example with more than one lambda
-seeds = [0, 1, 2]
 env = "CartPole-v1"
 
-# Create all combinations of lambda and seed
-combinations = [(gae_lambda, seed) for gae_lambda in lambdas for seed in seeds]
-
-# Run in parallel using threads
-with ThreadPoolExecutor(max_workers=len(combinations)) as executor:
-    futures = [executor.submit(test_ppo_lambda, env, gae_lambda, seed) for gae_lambda, seed in combinations]
-
-# Optionally wait for all to finish
-for future in futures:
-    future.result()
+test_ppo_lambda(env,0.95,0)
