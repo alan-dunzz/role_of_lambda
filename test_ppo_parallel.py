553bc2a4e4dadb2e3b56dbdc9042a8bbfa463f6e
PedroHGMS <pedrom072001@gmail.com>
2025-11-12 16:02:54 -0700

    PPO params change to args

--- test_ppo_parallel.py
+++ test_ppo_parallel.py
@@ -12,4 +12,5 @@ combinations = [(gae_lambda, seed) for gae_lambda in lambdas for seed in seeds]
 i = int(sys.argv[1])
 gae_lambda, seed = combinations[i]
 
+seed = int(seed)
 ppo_run(env_id=env,gae_lambda=gae_lambda,seed=seed)
