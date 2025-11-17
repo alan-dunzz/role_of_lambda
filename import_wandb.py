fd093c5025718d796a7113f435402b860715b5bd
Alan Dunzz <dunzzlla@ualberta.ca>
2025-11-17 15:22:00 -0600

    Updating runs and import code

--- import_wandb.py
+++ import_wandb.py
@@ -1,6 +1,7 @@
 import wandb
 import pandas as pd
 import os
+from tqdm import tqdm
 
 LOG_FILE = "completed_files.txt"
 
@@ -19,23 +20,29 @@ runs = api.runs("alan_dunzz-university-of-alberta/role_of_lambda")
 i = 0
 total = len(runs)
 
-for run in runs:
+for run in tqdm(runs, total=total):
     try:
         seed = run.config.get("seed")
         gae_lambda = run.config.get("gae_lambda")
-        print(fr"gae_lambda = {gae_lambda}, seed = {seed}")
-        df = pd.DataFrame(run.history())
-
-        out_dir = os.path.join(current_dir, "runs", f"lambda_{gae_lambda}")
-        os.makedirs(out_dir, exist_ok=True)
-
-        out_file = f"run_lambda_{gae_lambda}_seed_{seed}.csv"
+        if isinstance(gae_lambda, float):
+            lambda_clean = f"{gae_lambda:.3f}".rstrip("0").rstrip(".")
+        else:
+            lambda_clean = str(gae_lambda)
+        print(fr"gae_lambda = {lambda_clean}, seed = {seed}")
+        
+        out_file = f"run_lambda_{lambda_clean}_seed_{seed}.csv"
         i+=1
         if out_file in completed_files:
             print(f"[SKIP] {out_file} already exists. ({i}/{total})")
+            pass
         else:
+            df = pd.DataFrame(run.scan_history(keys=['charts/episodic_return','global_step']))
+            df.rename(columns={"charts/episodic_return": "episodic_return"}, inplace=True)
+            out_dir = os.path.join(current_dir, "runs", f"lambda_{lambda_clean}")
+            
+            os.makedirs(out_dir, exist_ok=True)
+            
             full_path = os.path.join(out_dir, out_file)
-
             df.to_csv(full_path, index=False)
             with open(LOG_FILE, "a") as f:
                 f.write(out_file + "\n")
