ec265c00f8470542130fc461d71f0e8fb2f48c16
PedroHGMS <pedrom072001@gmail.com>
2025-11-18 13:39:12 -0700

    Added script to github

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --account=aip-lelis
+#SBATCH --time=01:00:00
+#SBATCH --array=0-899
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=700M
+#SBATCH -o jobs_outputs/job-%A_%a.out
+#SBATCH -e jobs_outputs/job-%A_%a.err
+
+python test_ppo_parallel.py $SLURM_ARRAY_TASK_ID
