c406dc50f53fb7532d99c541a474cf22e2fa60c0
PedroHGMS <pedrom072001@gmail.com>
2025-11-19 11:21:12 -0700

    Averaging Script update

--- runs/generate_averages.py
+++ runs/generate_averages.py
@@ -19,7 +19,7 @@ lambda_folders = sorted([item.name for item in runs_folder.iterdir() if item.is_
 lambdas = [re.findall(r'\d*\.?\d+', lambda_folder)[-1] for lambda_folder in lambda_folders]
 
 # Load the csv for each lambda and each seed as a Pandas array
-return_per_timestep_for_each_lambda = pd.DataFrame(columns=lambdas)
+average_return_per_timestep_for_each_lambda = pd.DataFrame(columns=lambdas)
 for lambda_folder in lambda_folders:
     print(f'Reading folder: {runs_folder/lambda_folder}')
     
@@ -32,6 +32,7 @@ for lambda_folder in lambda_folders:
 
     # Interpolate and average different seeds
     averaged_interpolated_returns = np.zeros(500_000+1)
+    averaged_seeds = []
     for csv_path in same_lambda_different_seeds_csvs:
         # Reading CSV
         dataframe = pd.read_csv(csv_path)
@@ -42,16 +43,21 @@ for lambda_folder in lambda_folders:
 
         interpolated_values = np.interp(np.linspace(0, 500_000-1, 500_000+1), dataframe[x_col], dataframe[y_col])
 
-        # Averaging
+        # Averaging timestep returns over seeds
         averaged_interpolated_returns += interpolated_values/number_of_seeds
         if np.isnan(interpolated_values.mean()):
             print(csv_path)
+
+        # Averaging average returns of seeds
+        averaged_seeds.append(dataframe[y_col].mean())
+    
+    print(f'Average over seeds for lambda={labas}: {np.array(averaged_seeds).mean()}')
     # Storing the averaged returns for this lambda 
-    return_per_timestep_for_each_lambda[labas] = averaged_interpolated_returns
+    average_return_per_timestep_for_each_lambda[labas] = averaged_interpolated_returns
     print(f'Average for lambda={labas}: {averaged_interpolated_returns.mean()}')
 
 # Saving the final dataframe
 analysed_data_folder = 'runs/' + 'analysed_data'
 analysed_data_folder = Path(analysed_data_folder)
 analysed_data_folder.mkdir(exist_ok=True)
-return_per_timestep_for_each_lambda.to_csv(analysed_data_folder / f'average_return_per_timestep_for_each_lambda_{env_name}.csv', index=False)
+average_return_per_timestep_for_each_lambda.to_csv(analysed_data_folder / f'average_average_return_per_timestep_for_each_lambda_{env_name}.csv', index=False)
