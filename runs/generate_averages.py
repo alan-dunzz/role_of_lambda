d9c260178cbf9e219a33ef0fa7896b0b683c32d1
PedroHGMS <pedrom072001@gmail.com>
2025-11-19 11:55:33 -0700

    Array bug fix

--- runs/generate_averages.py
+++ runs/generate_averages.py
@@ -54,7 +54,7 @@ for lambda_folder in lambda_folders:
     confidence_interval_95_percent = 1.96 * (np.array(averaged_seeds).std() / np.sqrt(number_of_seeds))
     print(f'Average over seeds for lambda={labas}: {np.array(averaged_seeds).mean():.2f} +- {confidence_interval_95_percent:.2f}')
     # Storing the averaged returns for this lambda 
-    average_return_per_timestep_for_each_lambda[labas] = [*averaged_interpolated_returns, np.array(averaged_seeds).std()]
+    average_return_per_timestep_for_each_lambda[labas] = [*averaged_interpolated_returns, confidence_interval_95_percent]
     print(f'Average for lambda={labas}: {averaged_interpolated_returns.mean()}')
 
 # Saving the final dataframe
